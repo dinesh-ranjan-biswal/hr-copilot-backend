ea2ebe3113eeecd6ada67876107321044e76a4c5
dinesh-ranjan-biswal <dbiswal@gemini-us.com>
2025-01-31 15:59:27 +0530

    update the cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({origin:process.env.CORS_ORIGIN}));
+app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}));
 app.use(sessionCustom);
 app.use(`/${process.env.MAIN_ROUTE}`, apiRoute);
 
